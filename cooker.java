c0e296b488e697a991116379580d7137eaf134b1
mistyy <105160801+MistyyMistyy@users.noreply.github.com>
2023-07-09 00:38:25 -0500

    A new update for Blinmaker!
    All i did is improve the dialogue so it's less "borisey"

--- cooker.java
+++ cooker.java
@@ -12,22 +12,22 @@ public class cooker {
 		int milkMin = 200; //milliliter
 		int flourAmount;
 		int flourMin = 100; // grams
-		System.out.println("hello Boris!");
-		System.out.println("blinmaker is starting up..");
-		System.out.println("How many eggs you have?");
+		System.out.println("Hello user!!");
+		System.out.println("Blinmaker is starting! Ready to cook?");
+		System.out.println("How many eggs do you have?");
 		
 		Scanner userInput;
 		userInput = new Scanner(System.in);
 		eggsAmount = userInput.nextInt();
 		//System.out.println("you have " + eggsAmount + " eggs");
 		// eggs done
-		System.out.println("how much milk you have?");
+		System.out.println("How much milk(ML) do you have?");
 		userInput = new Scanner(System.in);
 		milkAmount = userInput.nextInt();
 		//System.out.println("you have " + milkAmount + "ml milk");
 		//milk done
 		
-		System.out.println("how much flour you have?");
+		System.out.println("how much flour do you have?");
 		userInput = new Scanner(System.in);
 		flourAmount = userInput.nextInt();
 		//System.out.println("you have " + flourAmount + "g flour");
@@ -58,13 +58,14 @@ public class cooker {
 			}
 				
 				System.out.println(" "); // every portion is 4 blins
-				System.out.println("you can make " + smallest*4 +  " portions of blins");
+				System.out.println("You can make " + smallest*4 +  " portions of blins");
 				System.out.println(" ");
 				System.out.println("you will need " + smallest*eggsMin + " eggs");
 				System.out.println("you will need " + smallest*milkMin + " ml of milk");
-				System.out.println("you will need " + smallest*flourMin + " g of flour");
+				System.out.println("you will need " + smallest*flourMin + " gr of flour");
 				System.out.println(" ");
-				System.out.println("blinmaker is shutting down");
+				System.out.println("Blinmaker is shutting down! Goodbye Friend");
+				System.out.println("*screams of horror*");
 				
 				
 						
